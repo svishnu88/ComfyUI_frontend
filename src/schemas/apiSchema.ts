e350f735a52a7ce7fb7376187e65e79943846940
bymyself <cbyrne@comfy.org>
2026-03-12 04:29:55 -0700

    feat: add focusedJobId for concurrent job execution support

--- src/schemas/apiSchema.ts
+++ src/schemas/apiSchema.ts
@@ -464,7 +464,9 @@ const zSettings = z.object({
   'Comfy.VersionCompatibility.DisableWarnings': z.boolean(),
   'Comfy.RightSidePanel.IsOpen': z.boolean(),
   'Comfy.RightSidePanel.ShowErrorsTab': z.boolean(),
-  'Comfy.Node.AlwaysShowAdvancedWidgets': z.boolean()
+  'Comfy.Node.AlwaysShowAdvancedWidgets': z.boolean(),
+  'Comfy.Cloud.ConcurrentExecution': z.boolean(),
+  'Comfy.Cloud.ConcurrentExecution.OnboardingSeen': z.boolean()
 })
 
 export type EmbeddingsResponse = z.infer<typeof zEmbeddingsResponse>
